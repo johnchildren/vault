8c6946728b48b667dd15a4de35c200b74d53d904
Jaymala Sinha <jaymala@hashicorp.com>
2023-04-19 17:05:28 -0400

    Update verify changes script
    Signed-off-by: Jaymala Sinha <jaymala@hashicorp.com>

--- .github/scripts/verify_changes.sh
+++ .github/scripts/verify_changes.sh
@@ -6,9 +6,9 @@ base_ref=$2
 changed_dir=""
 
 if [[ "$1" == "pull_request" ]]; then
-  change_count=$(git diff --merge-base $2 --name-only |xargs dirname |awk -F"/" '{ print $1}' |uniq |wc -l)
+  change_count=$(git diff --merge-base $2 --name-only |awk -F"/" '{ print $1}' |uniq |wc -l)
   if [[ $change_count -eq 1 ]]; then
-    changed_dir=$(git diff --merge-base $2 --name-only |xargs dirname |awk -F"/" '{ print $1}' |uniq)
+    changed_dir=$(git diff --merge-base $2 --name-only |awk -F"/" '{ print $1}' |uniq)
   fi
 else
   change_count=$(git show --oneline --dirstat=files,0 |grep "%" |awk '{print $2}' |awk -F"/" '{print $1}' |uniq |wc -l)
